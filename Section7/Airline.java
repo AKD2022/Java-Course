643ee45cc1ff4a8d65210da86220daeebf102193
unknown <akdusa20@gmail.com>
2022-07-28 17:59:58 -0400

    completed airline class

--- Section7/Airline.java
+++ Section7/Airline.java
@@ -1,3 +1,17 @@
 public class Airline {
     
+    Person[] seats;
+
+    public void setSeats(Person[] seats) {
+        this.seats = new Person[11];
+    }
+
+    public Person getPerson(int index){
+        return new Person(this.seats[index]);
+    }
+    
+    public void setPerson(Person person){
+        this.seats[person.getSeatNumber() - 1] = new Person(person);
+    }
+
 }
