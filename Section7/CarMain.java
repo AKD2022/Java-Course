510d63b4c2115923c3d9503286b55a6297a795de
unknown <akdusa20@gmail.com>
2022-07-29 13:44:08 -0400

    printed objects

--- Section7/CarMain.java
+++ Section7/CarMain.java
@@ -6,14 +6,14 @@ public class CarMain {
         Car[] cars = new Car[]{
             new Car("Nissan", 5000, 2020, "Red", new String[] {"Tires", "Keys"}),
             new Car("Dodge", 8500, 2019, "Black", new String[] {"Tires", "Keys"}),
-            new Car("Nissan", 5000, 2017, "Yellow", new String[] {"Tires", "Parts"})
+            new Car("Nissan", 5000, 2017, "Yellow", new String[] {"Tires", "Filters"}),
+            new Car("Honda", 7000, 2017, "Orange", new String[] {"Tires", "Filters"}),
+            new Car("Mercedes", 12000, 2015, "Jet Black", new String[] {"Tires", "Filters", "Transmission"}),
         };
 
-        Dealership dealership = new Dealership();
+        Dealership dealership = new Dealership(cars);
 
-        for (int i = 0; i < cars.length; i++) {
-            dealership.setCar(cars[i], i);
-        }
+        System.out.println(dealership);
 
    
 
