d98a983bcbece0a9782bbec1818243a13e006836
unknown <akdusa20@gmail.com>
2022-07-29 16:32:51 -0400

    made sure getter and setter are working

--- Section7/Machine.java
+++ Section7/Machine.java
@@ -1,4 +1,34 @@
 public class Machine {
+    private Item[][] items;
+
+
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+
+    public Item getItem (int row, int spot) {
+        return this.items[row][spot];
+    }
+
+    public Item setItem (Item item, int row, int spot) {
+        return this.items[spot][row] = new Item(item);
+    }
+
+
+
+
+
+
+
+
+
+
+
     
     /**
      * Function name – dispense
