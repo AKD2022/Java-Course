bdb9e27b1eefd1e42a9e7f0ad199c76d3349a807
unknown <akdusa20@gmail.com>
2022-07-29 14:01:33 -0400

    added more cars

--- Section7/CarMain.java
+++ Section7/CarMain.java
@@ -11,6 +11,15 @@ public class CarMain {
             new Car("Nissan", 5000, 2017, "Yellow", new String[] {"Tires", "Filters"}),
             new Car("Honda", 7000, 2017, "Orange", new String[] {"Tires", "Filters"}),
             new Car("Mercedes", 12000, 2015, "Jet Black", new String[] {"Tires", "Filters", "Transmission"}),
+            new Car("Bentley", 120000, 2022, "Jet Black", new String[] {"Tires", "Filters", "Transmission", "Mats"}),
+            new Car("Cadilac", 85000, 2019, "Black", new String[] {"Tires", "Filters", "Transmission", "Mats"}),
+            new Car("Mercedes", 30000, 2021, "Navy Blue", new String[] {"Tires", "Filters", "Transmission", "Mats"}),
+            new Car("Audi", 75000, 2022, "Navy Black", new String[] {"Tires", "Filters", "Transmission", "Mats", "Drivers Kit"}),
+            new Car("Toyota", 25000, 2022, "White", new String[] {"Tires", "Filters", "Transmission"}),
+            new Car("Ford", 20000, 2015, "Red", new String[] {"Tires", "Filters", "Transmission"}),
+            new Car("Chevy", 25000, 2015, "Blue", new String[] {"Tires", "Filters", "Transmission"}),
+            new Car("Mitshubishi", 38000, 2015, "Tan", new String[] {"Tires", "Filters", "Transmission"}),
+            new Car("Hyundai", 45000, 2015, "Navy Black", new String[] {"Tires", "Filters", "Transmission", "Mats", "Drivers Kit"}),
         };
 
         Dealership dealership = new Dealership(cars);
