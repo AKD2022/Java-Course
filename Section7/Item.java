5621b64556b26bd4447782e95340dcb76c4ace25
unknown <akdusa20@gmail.com>
2022-07-29 16:00:01 -0400

    added constuctor, getter ,setter

--- Section7/Item.java
+++ Section7/Item.java
@@ -1,3 +1,41 @@
 public class Item {
-    
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName (String name) {
+        return this.name;
+    }
+
+    public double getPrice (double price) {
+        return this.price;
+    }
+
+    public int getQuantity (int quantity) {
+        return this.quantity;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+    public void setQuantity (int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Item (Item source) {
+        this.name = source.name;
+        this.price = source.price;
+        this.quantity = source.quantity;
+    }
 }
