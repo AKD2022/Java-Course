e278271bf590a5c42cce87de66b13e04c97773d5
unknown <akdusa20@gmail.com>
2022-08-18 14:52:06 -0400

    commit

--- Section8/CheckedExceptions/ReadingFiles.java
+++ Section8/CheckedExceptions/ReadingFiles.java
@@ -1,5 +1,7 @@
-public class ReadingFiles {
+import java.io.FileInputStream;
+
+public class ReadingFiles throws FileNotFoundException {
     public static void main(String[] args) {
-                
+          FileInputStream fis = new FileInputStream("Greetings.txt");      
     }
 }
