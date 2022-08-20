c032d5ea78db3336fe8670d957c221fe4fb24014
unknown <akdusa20@gmail.com>
2022-08-20 14:36:13 -0400

    finished lesson

--- Section8/ImmutableWrappers/Wrapper.java
+++ Section8/ImmutableWrappers/Wrapper.java
@@ -1,8 +1,10 @@
 public class Wrapper {
     public static void main(String[] args) {
-        int wholeNumber = 5;
-        long largeWholeNumber = 5000000000L;
-        double decimal = 5.493;
-        char letter = 'b';
+        Integer wholeNumber = 5;
+        Integer numberTwo = wholeNumber;
+        numberTwo = 10;
+        Long largeWholeNumber = 5000000000L;
+        Double decimal = 5.493;
+        Character letter = 'b';
     }
 }
