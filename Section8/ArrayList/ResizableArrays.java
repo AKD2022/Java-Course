33e3c73a43ee962d4afc5fb127d177ecb6e55103
unknown <akdusa20@gmail.com>
2022-08-20 19:16:17 -0400

    commit

--- Section8/ArrayList/ResizableArrays.java
+++ Section8/ArrayList/ResizableArrays.java
@@ -6,8 +6,11 @@ public class ResizableArrays {
         
         names.add("Joe");
         names.add("Jim");
-        names.add("John");
+        names.add("John");  
         
+
+        
+
         names.size();
         for (int i = 0; i < names.size(); i++) {
             
