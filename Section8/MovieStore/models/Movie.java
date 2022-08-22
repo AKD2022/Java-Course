a8a8087c0057eb67ab6cb5b51cbd33e6e73675be
unknown <akdusa20@gmail.com>
2022-08-22 14:02:28 -0400

    added classes

--- Section8/MovieStore/models/Movie.java
+++ Section8/MovieStore/models/Movie.java
@@ -1,5 +1,12 @@
 package models;
 
 public class Movie {
+    private String name;
+    private String format;
+    private int rating;
+    private double sellingPrice;
+    private double rentingPrice;
+    private boolean isAvailable;
+
     
 }
