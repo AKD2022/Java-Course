aa75d21c688052d0bd66268e0b92b3495f3055a7
unknown <akdusa20@gmail.com>
2022-10-07 20:32:47 -0400

    opened lesson folder in own workspace

--- Section9/Intro-unit_testing/src/main/Main.java
+++ Section9/Intro-unit_testing/src/main/Main.java
@@ -1,11 +1,16 @@
+package src.main;
 
 public class Main {
     
     static double[] prices = new double[] {2.23, 1.32, 4.32, 11.33};
 
     public static void main(String[] args) {   
+        
 
+    }
 
+    public static double getSubtotal() {
+        return 0;
     }
 
 }
--- Section9/Intro-unit_testing/src/test/checkoutTest.java
+++ Section9/Intro-unit_testing/src/test/checkoutTest.java
@@ -1,3 +1,13 @@
+package src.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import src.main.Main;
+import org.junit.Test;
+
+
 public class checkoutTest{
-    
+    @Test
+    public void subtotalIsValid() {
+        assertEquals(19.2, Main.getSubtotal());
+    }
 }
