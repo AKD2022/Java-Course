0f399fdc4f4634b3b95b89325fe1fd6178d13dee
AKD <akdusa20@gmail.com>
2023-07-24 14:51:53 -0400

    completed challenge one

--- Workbooks/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Workbooks/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("      A             DDDDDD");
+         System.out.println("     A  A           D      D");
+         System.out.println("    A    A          D       D");
+         System.out.println("   AAAAAAAA         D        D");
+         System.out.println("  A        A        D      D");
+         System.out.println(" A          A       D     D");
+         System.out.println("A            A      DDDDD");
     }
 }
