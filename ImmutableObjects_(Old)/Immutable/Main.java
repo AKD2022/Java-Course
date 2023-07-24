009f16ac5569a29a5eef86534ef1a7be88964344
AKD <akdusa20@gmail.com>
2023-07-24 14:17:18 -0400

    deleted java course files restarting...

--- ImmutableObjects_(Old)/Immutable/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Ryan", "Canadian"); // Mutable or Immutable?
-        Person person2 = person;
-        person2 = new Person("John", "American");
-    }
-}
