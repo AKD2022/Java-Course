b45d4650f4cf0ff27105fab9d715b188a04bc6f0
AKD <akdusa20@gmail.com>
2023-07-30 21:11:51 -0400

    finished lesson

---
+++ Section Two/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 8_000_000_000L; /* Underscore is for comma (which doesn't work in code) */
+        System.out.println("The global population is " + globalPopulation);
+
+        long dailyGoogleSearches = 8_500_000_000L; /* Underscore is for comma (which doesn't work in code) */
+        System.out.println("Daily there are " + dailyGoogleSearches + " google searches");
+    }
+}
